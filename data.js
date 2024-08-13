f7f487fdeaecc7368ad3103871d98946c4cd3cdb
Amina-Akada <amina.amirjani@gmail.com>
2024-08-13 18:40:22 +0500

    JS file

---
+++ data.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+      id: 1,
+      question: "Lorem ipsum dolor sit amet?",
+      answer:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, eius porro!",
+    },
+    {
+      id: 2,
+      question: "Lorem ipsum dolor sit amet?",
+      answer:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, eius porro!",
+    },
+    {
+      id: 3,
+      question: "Lorem ipsum dolor sit amet?",
+      answer:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, eius porro!",
+    },
+    {
+      id: 4,
+      question: "Lorem ipsum dolor sit amet?",
+      answer:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, eius porro!",
+    },
+    {
+      id: 5,
+      question: "Lorem ipsum dolor sit amet?",
+      answer:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, eius porro!",
+    },
+  ];
